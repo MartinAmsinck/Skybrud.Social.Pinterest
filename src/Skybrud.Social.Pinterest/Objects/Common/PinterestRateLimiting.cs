73359f089605c836c2e714bdb8bb672da1fabffa
Anders Bjerner <abjerner@skybrud.dk>
2016-12-31 17:22:35 +0100

    Added extra property to all PinterestResponse with rate limiting information

---
+++ src/Skybrud.Social.Pinterest/Objects/Common/PinterestRateLimiting.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Skybrud.Social.Http;
+
+namespace Skybrud.Social.Pinterest.Objects.Common {
+    
+    /// <summary>
+    /// Class with rate-limiting information about a response from the Pinterest API.
+    /// </summary>
+    public class PinterestRateLimiting {
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the total number of calls allowed within the current 1-hour window.
+        /// </summary>
+        public int Limit { get; private set; }
+
+        /// <summary>
+        /// Gets the remaining number of calls available to your app within the current 1-hour window.
+        /// </summary>
+        public int Remaining { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        private PinterestRateLimiting(int limit, int remaining) {
+            Limit = limit;
+            Remaining = remaining;
+        }
+
+        #endregion
+
+        #region Static methods
+
+        /// <summary>
+        /// Parses rate-limiting information from the specified <see cref="response"/>.
+        /// </summary>
+        /// <param name="response">The response that holds the rate-limiting information.</param>
+        /// <returns>Returns an instance of <see cref="PinterestRateLimiting"/>.</returns>
+        public static PinterestRateLimiting GetFromResponse(SocialHttpResponse response) {
+
+            int limit;
+            int remaining;
+
+            if (!Int32.TryParse(response.Headers["X-Ratelimit-Limit"] ?? "", out limit)) {
+                limit = -1;
+            }
+
+            if (!Int32.TryParse(response.Headers["X-Ratelimit-Remaining"] ?? "", out remaining)) {
+                remaining = -1;
+            }
+
+            return new PinterestRateLimiting(limit, remaining);
+
+        }
+
+        #endregion
+
+    }
+
+}
--- src/Skybrud.Social.Pinterest/Responses/PinterestResponse.cs
+++ src/Skybrud.Social.Pinterest/Responses/PinterestResponse.cs
@@ -2,6 +2,7 @@
 using Skybrud.Social.Http;
 using Skybrud.Social.Pinterest.Exceptions;
 using Skybrud.Social.Pinterest.Objects;
+using Skybrud.Social.Pinterest.Objects.Common;
 
 namespace Skybrud.Social.Pinterest.Responses {
 
@@ -10,13 +11,24 @@ namespace Skybrud.Social.Pinterest.Responses {
     /// </summary>
     public abstract class PinterestResponse : SocialResponse {
 
+        #region Properties
+
+        /// <summary>
+        /// Gets information about rate limiting.
+        /// </summary>
+        public PinterestRateLimiting RateLimiting { get; private set; }
+
+        #endregion
+
         #region Constructor
 
         /// <summary>
         /// Initializes a new instance from the specified <paramref name="response"/>.
         /// </summary>
         /// <param name="response">The raw response the instance should be based on.</param>
-        protected PinterestResponse(SocialHttpResponse response) : base(response) { }
+        protected PinterestResponse(SocialHttpResponse response) : base(response) {
+            RateLimiting = PinterestRateLimiting.GetFromResponse(response);
+        }
 
         #endregion
 
